be642b86077fb0d170f49306ced5e535470db587
Riko Mari <62793251+marick-py@users.noreply.github.com>
2022-11-19 00:09:19 +0100

    Add files via upload

--- e2D.py
+++ e2D.py
@@ -85,7 +85,7 @@ class Vector2D:
             if 0 <= t <= 1 and 0 <= u <= 1:
                 return round(P1[0] * t + P0[0] * (1-t)), round(P1[1] * t + P0[1] * (1-t))
             return None
-        collisions = [V2(info=line) for line in [lineLineIntersect(line1[1](), line1[0](), ray[:2], ray[2:]) for line1 in lines] if line and not return_empty]
+        collisions = [V2(info=line) for line in [lineLineIntersect(line1[1](), line1[0](), ray[:2], ray[2:]) for line1 in lines] if line or not return_empty]
         if sort:
             collisions.sort(key=lambda x: self.distance_to(x, False))
         return collisions
