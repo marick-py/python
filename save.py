1b94cf6973a1d66691d6d3545513d28d2a52ee7d
Riko Mari <62793251+marick-py@users.noreply.github.com>
2022-11-19 01:38:03 +0100

    Add files via upload

--- save.py
+++ save.py
@@ -1,8 +1,11 @@
 import itertools
 import pickle
 import os
+import sys
 
 max_file_size = 10_000
+
+_splitter = "\\" if sys.platform == "win32" else "/"
 
 def load(path):
     ls = os.listdir(path)
@@ -11,11 +14,11 @@ def load(path):
         if ".pickle" in file:
             with open(path+file, 'rb') as f:
                 qt.update(pickle.load(f))
-        pprint(n+1, len(ls), path.split("/")[-1])
+        pprint(n+1, len(ls), path.split(_splitter)[-1])
     return qt
 
 def pprint(start, end, name):
-    os.system("clear")
+    os.system("cls")
     print(name)
     print(str(round(100/end*start))+f" %\n{start} / {end}")
 
@@ -27,10 +30,10 @@ def save(path, qt):
     div = divmod(len(qt), max_file_size)
     i = 0
     for i in range(div[0]):
-        with open(path+f"/qt_{str(i)}.pickle", "wb") as f:
+        with open(path+f"{_splitter}qt_{str(i)}.pickle", "wb") as f:
             pickle.dump(dict(itertools.islice(qt.items(), max_file_size*i, max_file_size+max_file_size*i)),f, pickle.HIGHEST_PROTOCOL)
-        pprint(i+1, div[0]+1, path.split("/")[-1])
+        pprint(i+1, div[0]+1, path.split(_splitter)[-1])
     if div[1] > 0:
-        with open(path+f"/qt_{str(i+1)}.pickle", "wb") as f:
+        with open(path+f"{_splitter}qt_{str(i+1)}.pickle", "wb") as f:
             pickle.dump(dict(itertools.islice(qt.items(), len(qt)-div[1], len(qt))),f, pickle.HIGHEST_PROTOCOL)
-        pprint(i+(1 if div[0]==0 else 2), div[0]+1, path.split("/")[-1])
+        pprint(i+(1 if div[0]==0 else 2), div[0]+1, path.split(_splitter)[-1])
